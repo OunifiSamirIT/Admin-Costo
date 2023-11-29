057534f5ae3f1d7a5f58859b9089228dbe569889
OunifiSamirIT <ounifisamiritdsi@gmail.com>
2023-11-29 02:01:20 +0000

    fix navigation

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { useRoutes, Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import SideMenu from "../layouts/side-menu/Main";
-import Login from "../views/Auth/Login";
-import Register from "../views/Auth/register";
-import Dashboard from "../views/Dashboard/Main";
-import Products from "../views/Products/Main";
-import Categories from "../views/Categories/Main";
-import Transactions from "../views/Transactions/Main";
-import Settings from "../views/Settings/Main";
+import React from 'react';
+import { useRoutes, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import SideMenu from '../layouts/side-menu/Main';
+import Login from '../views/Auth/Login';
+import Register from '../views/Auth/register';
+import Dashboard from '../views/Dashboard/Main';
+import Products from '../views/Products/Main';
+import Categories from '../views/Categories/Main';
+import Transactions from '../views/Transactions/Main';
+import Settings from '../views/Settings/Main';
 
 function AuthenticatedRoutes() {
   return (
@@ -23,44 +23,18 @@ function AuthenticatedRoutes() {
 }
 
 function Router() {
-  const isAuthenticated = useSelector(
-    (state) => state.auth.status === "succeeded"
-  );
-  const isLoggingIn = useSelector((state) => state.auth.status === "loading");
-  if (isLoggingIn) {
-    return <div>Checking Authentication...</div>;
-  }
-
-  if (!isAuthenticated) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="text-center">
-          <div className="animate-spin text-9xl text-indigo-600 mb-2">
-            &#9696;
-          </div>
-          <div className="text-gray-700 text-5xl">
-            Not Authorized. Please log in.
-          </div>
-        </div>
-      </div>
-    );
-  }
-
+  const isAuthenticated = useSelector((state) => state.auth.status === 'succeeded');
   const routes = useRoutes([
     {
-      path: "/",
-      element: isAuthenticated ? (
-        <AuthenticatedRoutes />
-      ) : (
-        <Navigate to="/login" />
-      ),
+      path: '/',
+      element: isAuthenticated ? <AuthenticatedRoutes /> : <Navigate to="/login" />,
     },
     {
-      path: "/login",
+      path: '/login',
       element: <Login />,
     },
     {
-      path: "/register",
+      path: '/register',
       element: <Register />,
     },
   ]);
--- src/router/privateroute.jsx
+++ src/router/privateroute.jsx
@@ -1,15 +1,15 @@
-// PrivateRoute.js
 import React from "react";
 import { Route, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const PrivateRoute = ({ children, ...rest }) => {
+const PrivateRoute = ({ element, ...rest }) => {
   const isAuthenticated = useSelector((state) => state.auth.user !== null);
 
-  return isAuthenticated ? (
-    <Route {...rest}>{children}</Route>
-  ) : (
-    <Navigate to="/login" />
+  return (
+   <> <Route
+      {...rest}
+      element={isAuthenticated ? element : <Navigate to="/login" />}
+    /></>
   );
 };
 
