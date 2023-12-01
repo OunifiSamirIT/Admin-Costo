48565baf722a7d13262d0f465d71891cf3a400c1
Nader <ghazouani.nader@gmail.com>
2023-12-01 22:47:15 +0100

    Revert "fix refresh page"
    This reverts commit 0bb963e0ae47cfd665793eeb15c095941a25514b.

--- src/redux/Reducer/authSlice.js
+++ src/redux/Reducer/authSlice.js
@@ -1,4 +1,3 @@
-
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
@@ -8,75 +7,68 @@ const initialState = {
   error: null,
 };
 
-// Try to get user data from local storage
-const storedUser = JSON.parse(localStorage.getItem('user'));
-
-if (storedUser) {
-  initialState.user = storedUser;
-}
-
 export const registerUser = createAsyncThunk('auth/register', async (userData) => {
   const response = await axios.post('http://localhost:5000/api/auth/register', userData);
   return response.data;
 });
 
 export const loginUser = createAsyncThunk('auth/login', async (credentials) => {
-  const response = await axios.post('http://localhost:5000/api/auth/login', credentials);
-  return response.data;
-});
+    const response = await axios.post('http://localhost:5000/api/auth/login', credentials);
+    return response.data;
+  });
 
-export const logoutUser = createAsyncThunk('auth/logout', async () => {
-  await axios.post('http://localhost:5000/api/auth/logout');
-});
+  export const logoutUser = createAsyncThunk('auth/logout', async () => {
+    // You may want to clear user data from local storage or perform other cleanup here
+    // ...
+  
+    // Call the logout endpoint on the server
+    await axios.post('http://localhost:5000/api/auth/logout');
+  });
 
+  
 const authSlice = createSlice({
   name: 'auth',
   initialState,
-  reducers: {
-
-    setUser: (state, action) => {
-      state.user = action.payload;
-      localStorage.setItem('user', JSON.stringify(action.payload));
-    },
-  },
+  reducers: {},
   extraReducers: (builder) => {
     builder
-      // register
-      .addCase(registerUser.pending, (state) => {
+   //register
+    .addCase(registerUser.pending, (state) => {
         state.status = 'loading';
       })
       .addCase(registerUser.fulfilled, (state, action) => {
         state.status = 'succeeded';
         state.user = action.payload;
-        // Save user data to local storage
-        localStorage.setItem('user', JSON.stringify(action.payload));
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       })
 
-      // login
+//login
       .addCase(loginUser.pending, (state) => {
         state.status = 'loading';
       })
       .addCase(loginUser.fulfilled, (state, action) => {
         state.status = 'succeeded';
         state.user = action.payload.user;
-        // Save user data to local storage
-        localStorage.setItem('user', JSON.stringify(action.payload.user));
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.status = 'failed';
         state.error = action.error.message;
       })
-      // logout
+      //logout
       .addCase(logoutUser.fulfilled, (state) => {
         state.status = 'idle';
         state.user = null;
-        // Remove user data from local storage
-        localStorage.removeItem('user');
-      });
+      })
+  },
+
+
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
   },
 });
 export const { setUser } = authSlice.actions;
